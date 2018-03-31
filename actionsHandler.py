90447cea53e3d8b9397b48e66bc010604ab72746
nuclearnic <nicholas.erasmus@jumo.world>
2018-03-31 18:22:44 +0200

    refactor

--- actionsHandler.py
+++ actionsHandler.py
@@ -1,34 +1,8 @@
 def createResponse(template, parameters):
 
-  # template looks like:
-  # {'params': ['color', 'number'],
-  #   'response': {
-  #     'speech': 'Alright, your silly name is _color_ _number_! I hope you like it! See you next time.',
-  #     'displayText': 'Alright, your silly name is _color_ _number_! I hope you like it! See you next time.'
-  #   }
-  # }
-  # parameters looks like:
-  # 'parameters': {'color': 'red', 'given-name': '', 'number': 99},
-
-  # how to gsub in python
-  # replace('_' + x + '_', 'variable')
-
-  #  
-  print 'INSIDE CREATE RESPONSE'
-  print 'INSIDE CREATE RESPONSE'
-  print 'INSIDE CREATE RESPONSE'
-
   for param in template['params']:
-    # template[param] = template[param].replace('_' + param + '_',)
-    # do replace
+    # Handle each type of response
+    for response in template['response']:
+      template['response'][response] = template['response']['speech'].replace('_' + param + '_', str(parameters[param]))
 
-    print 'param:'
-    print param
-    print 'template[resp][speech]'
-    print template['response']['speech']
-    print 'String replacement:'
-    # print template['response']['speech'].replace('_' + param + '_', parameters[param])
-
-    template['response']['speech'] = template['response']['speech'].replace('_' + param + '_', str(parameters[param]))
-
-  return template
+  return template['response']
--- pythonResponder.py
+++ pythonResponder.py
@@ -18,55 +18,18 @@ class S(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'application/json')
         self.end_headers()
         
-    # How about making this a little bit dynamic by...
-    # 1. having a hash with:
-    #               action as the key
-    #               parameters key with params values
-    #               optional response text
-    #               potentially set intent ? 
-
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
         input = json.loads(post_data)
-        
-        # The ACTION name from the make_name Dialogflow intent, change this if needed
-        # NAME_ACTION = 'make.name' 
-        # The parameters that are parsed from the make_name INTENT, change these if needed
-        # COLOR_ARGUMENT = 'color'
-        # NUMBER_ARGUMENT = 'number'
-
+        print "Request body: %s" %(input)
         # Only run method if action matches the action we are needing in Dialogflow
         if input['result']['action'] in available_actions: 
-        # if NAME_ACTION == input['result']['action']:
             print "Action Matched"
             self.send_response(200)
             self._set_headers()
-
-            # action = actions[input['result']['action']]
-            # action = actions[input['result']['action']]
-
             response = actionsHandler.createResponse(available_actions[input['result']['action']], input['result']['parameters'])
-            # response = actionsHandler.createResponse(action, input['result']['parameters'])
-            # response = actionsHandler.createResponse(action['response'], input['result']['parameters'])
-
-            print "RESPONSE:"
-            print response
-
-            # BEFORETIME:
-            color = input['result']['parameters']['color']
-            number = input['result']['parameters']['number']
-            # AFTERTIME:
-            # for param in actions['params']:
-                
-
-            # OPTIONAL overwrite of Text Responses configured in DialogFlow
-            json_output = json.dumps(response['response'])
-            # json_output = json.dumps({'speech': 'Alright, your silly name is %s %s! I hope you like it! See you next time.'%(color,number)})
-            # json_output = json.dumps({'speech': 'Alright, your silly name is %s %s! I hope you like it! See you next time.'%(color,number),
-                # 'displayText': 'Alright, your silly name is %s %s! I hope you like it! See you next time.'%(color,number)})
-            # json_output = json.dumps({'sppech':})
-
+            json_output = json.dumps(response)
             self.wfile.write(json_output)
         else:
             print "No action matching %s" %(NAME_ACTION)
